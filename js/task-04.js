d2ffe128feee96ace95be92260b19d5c2def6550
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-20 17:05:31 +0300

    Add task 04

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,26 @@
-"use strict";
+'use strict';
+const countTotalSalary = function (employees) {
+  const values = Object.values(employees);
+  let totalSalary = 0;
+  for (const value of values) {
+    totalSalary += value;
+  }
+  return totalSalary;
+};
+console.log(countTotalSalary({}));
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+);
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+);
