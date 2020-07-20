9d519527ae8915ab37c76867e7577b23ce7f2af1
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-20 13:09:58 +0300

    Add task 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+user.mood = 'happy';
+user.hobby = 'skydaving';
+user.premium = false;
+
+const keys = Object.keys(user);
+/* Use for of */
+for (const key of keys) {
+  console.log(`${key} : ${user[key]}`);
+}
+
+/* Use for in without Object.keys */
+console.log('____________________________');
+
+for (const key in user) {
+  console.log(`${key} : ${user[key]}`);
+}
